7342bcf1b36d433fe9fd4f3b1bf92cc4a0516dbc
Unknown <anamargaridarl@gmail.com>
2018-12-09 19:55:56 +0000

    otimizaçao

--- TP6/tp6/tp6/tpp6gauss.cpp
+++ TP6/tp6/tp6/tpp6gauss.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 bool checkdivergence(double a1, double a2, double a3)
 {
-	if (abs(a2 + a3 / a1) < 1)
+	if (abs((a2 + a3) / a1) < 1)
 		return true;
 	else
 		return false;
@@ -15,28 +15,28 @@ bool checkdivergence(double a1, double a2, double a3)
 
 double f1(double x2, double x3)
 {
-	return (7 - x2 - x3) / 3;
+	return (12 - 1*x2 - 2*x3) /4 ;
 }
 
 double f2(double x1, double x3)
 {
-	return (4 - 2*x3 - x1) / 3;
+	return (13 - 2*x1 - 1*x3) / 4;
 }
 
-double f3(double x2)
+double f3(double x2,double x1)
 {
-	return (5 - 2 * x2) / 5;
+	return (16 - 2*x1 - x2) / 4;
 }
 
-double GaussJacobi(double x0, double y0, double z0)
-{
-	double x, y, z;
 
+void GaussJacobi(double x0, double y0, double z0, double &x, double &y, double &z)
+{
+	int n = 0;
 	x = f1(y0, z0);
 	y = f2(x0, z0);
-	z = f3(y0);
+	z = f3(y0,x0);
 
-	while ((x - x0 < 1e-6) && (y - y0 < 1e-6) && (z - z0 < 1e-6))
+	while ( abs(x-x0) > 0.01 && abs(y - y0) > 0.01 && abs(z - z0) > 0.01)
 	{
 		x0 = x;
 		y0 = y;
@@ -44,38 +44,54 @@ double GaussJacobi(double x0, double y0, double z0)
 
 		x = f1(y0, z0);
 		y = f2(x0,z0);
-		z = f3(y0);
-	
+		z = f3(y0,x0);
+		
+		cout << "x " << x << endl;
+		cout << "y " << y << endl;
+		cout << "z " << z << endl;
+		n++;
 	}
 }
 
-/*
-double GaussSeidel(double x0, double y0, double z0)
-{
-	double x, y, z;
 
+void GaussSeidel(double x0, double y0, double z0, double &x, double &y, double &z)
+{
 	x = f1(y0, z0);
 	y = f2(x0, z0);
-	z = f3(y0);
+	z = f3(y0,x0);
 
-	while ((x - x0 < 1e-6) && (y - y0 < 1e-6) && (z - z0 < 1e-6))
+	while (abs(x - x0) > 0.01 && abs(y - y0) > 0.01 && abs(z - z0) > 0.01)
 	{
 		x0 = x;
 		y0 = y;
 		z0 = z;
 
 		x = f1(y0, z0);
-		y = f2(x0, z0);
-		z = f3(y0);
+		y = f2(x, z0);
+		z = f3(y,x);
 
 	}
 }
-*/
+
 int main() {
+	
+	double x = 0;
+	double y = 0;
+	double z = 0;
 
+	//GaussJacobi(0, 0, 0, x, y, z);
+	GaussSeidel(0, 0, 0, x, y, z);
 
+	if (checkdivergence(4, 2, 1) == true)
+		cout << "1" << endl;
+	if(checkdivergence(1, 3, 1) == true)
+		cout << "2 " << endl;
+	if (checkdivergence(2, 1, 5) == true)
+	cout << "3" << endl;
 
-
+	cout << "x: " << x << endl;
+	cout << "y: " << y << endl;
+	cout << "z: " << z << endl;
 
 
 
